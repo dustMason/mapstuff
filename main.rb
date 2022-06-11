f049765d1fe6bb8893229165baba26955ac7c437
jordan <jordan@fiftyfootfoghorn.com>
2022-06-11 14:41:55 -0700

    need the geoJSON type

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
 require 'json'
 
-puts JSON.dump(Hash[JSON.parse(ARGF.read)["features"].map { |f| [f["properties"]["ISO3166-2"], f["geometry"]["coordinates"]] }])
+puts JSON.dump(Hash[JSON.parse(ARGF.read)["features"].map do |f|
+  [f["properties"]["ISO3166-2"], [f["geometry"]["coordinates"], f["geometry"]["type"]]]
+end])
